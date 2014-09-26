55b704998c483e7ae118c7034f81a89a2ec2419f
Shivaprasath <shiva.github@gmail.com>
2014-09-26 17:14:09 +0530

    Button
    This is a simple program to explain the functionality of button with
    Onclick Listener

---
+++ src/com/example/button/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.button;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity 
+{
+	// Define and declare a button here 
+	Button b;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+    //Refer the button from xml with the id of button b1 which is "button1"
+	b=(Button)findViewById(R.id.button1);
+	
+	// Set the listener for button , i'm using on click listener for button
+	b.setOnClickListener(new OnClickListener()
+	{
+		
+		@Override
+		public void onClick(View arg0)
+		{
+			// TODO Auto-generated method stub
+			
+			//The message to shown in toast when user clicks on the button
+			Toast.makeText(MainActivity.this,"You have cliked a button",Toast.LENGTH_LONG).show();
+			
+			
+		}
+	});
+	
+		
+	}
+
+
+
+}
